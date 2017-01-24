18b01360ee69bd1c7b85c08a5edbf91420042f16
phantamanta44 <evandalong@gmail.com>
2017-01-23 18:27:08 -0600

    Intro section plus fallback audio files

--- static/js/main.js
+++ static/js/main.js
@@ -3,16 +3,38 @@ $(document).ready(() => {
 
     const ls = $("#ls");
     const miracle = new Howl({
-        src: ["static/ogg/miracle.ogg"],
+        src: ["static/ogg/miracle.ogg", "static/mp3/miracle.mp3"],
         loop: true
+    });
+    const intro = new Howl({
+        src: ["static/ogg/intro.ogg", "static/mp3/intro.mp3"]
     });
     const dean = $("#dean-container");
     const lights = $("#light-container");
     const scores = $("#score-container");
+    let scoreCache = [];
+    let scoreWait = 3300;
 
-    const miraclePartTwo = () => {
+    const miraclePartThree = () => {
+        miracle.play();
         dean.addClass("our-saviour-is-coming");
         window.setTimeout(() => lights.addClass("shiny"), 5050);
+    };
+
+    const addScoreCard = () => {
+        let score = scoreCache.pop();
+        let card = $("<div>", {"class": "score"});
+        if (score.red)
+            card.addClass("red");
+        card.text(score.score);
+        card.css("left", Math.random() * 100 + "%");
+        scores.append(card);
+    };
+
+    const miraclePartTwo = () => {
+        addScoreCard();
+        if (scoreCache.length > 0)
+            window.setTimeout(miraclePartTwo, scoreWait);
     };
 
     const timeForAMiracle = () => {
@@ -20,12 +42,12 @@ $(document).ready(() => {
         $("#github").addClass("black");
         window.setTimeout(() => ls.addClass("pls"), 5050);
         window.setTimeout(miraclePartTwo, 1700);
-        miracle.play();
+        intro.play();
     };
 
     let readies = 0;
     const markReady = () => {
-        if (++readies >= 2) {
+        if (++readies >= 3) {
             ls.addClass("robot-is-ready");
             window.setTimeout(timeForAMiracle, 1200);
         }
@@ -33,6 +55,8 @@ $(document).ready(() => {
     };
 
     miracle.once("load", markReady);
+    intro.once("load", markReady);
+    intro.once("end", miraclePartThree);
 
     const addLightBeam = rotation => {
         let light = $("<div>", {"class": "light"});
@@ -43,22 +67,14 @@ $(document).ready(() => {
     for (let i = 0; i < 12; i++)
         addLightBeam(i * 30);
 
-    const addScoreCard = (score, red) => {
-        let card = $("<div>", {"class": "score"});
-        if (red)
-            card.addClass("red");
-        card.text(score);
-        card.css("animation-delay", Math.random() * 3.3 + "s");
-        card.css("left", Math.random() * 100 + "%");
-        scores.append(card);
-    };
-
     getEvents(new Date().getFullYear() - 1, events => {
         getScores(events[Math.floor(Math.random() * events.length)], eventScores => {
             for (let i = 0; i < eventScores.length && i < 15; i++) {
-                addScoreCard(eventScores[i].red, true);
-                addScoreCard(eventScores[i].blue, false);
+                scoreCache.push({score: eventScores[i].red, red: true});
+                scoreCache.push({score: eventScores[i].blue, red: false});
             }
+            scoreWait = 3300 / scoreCache.length;
+            console.log(scoreWait);
             markReady();
         });
     });
